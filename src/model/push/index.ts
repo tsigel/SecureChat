521384c9556ee00977ca45b63bea9c77bd522c3d
tsigel <tsddaniil@gmail.com>
2026-03-04 11:09:34 +0300

    Add debugger

--- src/model/push/index.ts
+++ src/model/push/index.ts
@@ -44,6 +44,7 @@ export const $notificationPermission = appD
     .on(refreshNotificationPermission, (state) => state);
 
 export const ensurePushSubscriptionFx = appD.createEffect(async () => {
+    debugger
     if (!canUsePush()) {
         throw new Error('Push API не поддерживается в этом браузере');
     }
